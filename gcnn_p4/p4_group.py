95e36b2fe30294d6e5947c996b154921734a6c13
Biiiilly <bm221@ic.ac.uk>
2024-11-05 11:42:05 +0000

    setup

---
+++ gcnn_p4/p4_group.py
@@ -0,0 +1,9 @@
+'''Define p4-group'''
+import numpy as np
+
+def p4_generator(r, u, v):
+    '''
+    The function used to generate p4 group.
+
+    
+    '''
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='gcnn_p4',
+    version='0.1.1',
+    description='Group equivariant convolutional neural networks in p4 group',
+    packages=[],
+)
