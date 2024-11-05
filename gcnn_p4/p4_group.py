063f806924a833fcfa45acc6411dd49a246f8438
Biiiilly <bm221@ic.ac.uk>
2024-11-05 12:11:43 +0000

    p4_generator

--- gcnn_p4/p4_group.py
+++ gcnn_p4/p4_group.py
@@ -5,5 +5,18 @@ def p4_generator(r, u, v):
     '''
     The function used to generate p4 group.
 
-    
+    :param r: an int representing rotation coefficient between 0 and 4.
+    :param u: an int representing horizontal translation.
+    :param v: an int representing vertical translation.
+
+    :return g: an 3x3 numpy array representing the element in p4.
     '''
+
+    g = np.zeros((3,3))
+    angle = r*np.pi / 2
+
+    g[:, 0] = [np.cos(angle), np.sin(angle), 0]
+    g[:, 1] = [-np.sin(angle), np.cos(angle), 0]
+    g[:, 2] = [u, v, 1]
+
+    return g
