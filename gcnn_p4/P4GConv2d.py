ee28d4cccbdcf838a6fa62fa1a6522fd9486f749
Biiiilly <bm221@ic.ac.uk>
2024-11-18 21:00:33 +0000

    group

--- gcnn_p4/P4GConv2d.py
+++ gcnn_p4/P4GConv2d.py
@@ -2,11 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class P4GConv2d(torch.nn.Module):
+class Z2P4GConv2d(torch.nn.Module):
 
     def __init__(self, in_channels, out_channels, kernel_size):
 
-        super(P4GConv2d, self).__init__()
+        super(Z2P4GConv2d, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         # Say kernel_size = n if the kernel has size nxn
@@ -24,17 +24,42 @@ class P4GConv2d(torch.nn.Module):
             output = F.conv2d(x, rotated_weight, bias=self.bias, padding='same')
             outputs.append(output)
 
-        gconv = torch.stack(outputs, dim=1)
-        gconv_output = gconv.view(gconv.size()[0], gconv.size()[1] * gconv.size()[2],
-                                    gconv.size()[3], gconv.size()[4])
-        return gconv_output
+        return torch.stack(outputs, dim=2)
     
 
-class P4GConvNet(nn.Module):
+class P4P4GConv2d(torch.nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size):
+
+        super(P4P4GConv2d, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size 
+
+        self.weight = torch.nn.Parameter(torch.randn(out_channels, in_channels * 4, kernel_size, kernel_size))
+        self.bias = torch.nn.Parameter(torch.randn(out_channels))
+
+    def forward(self, x):
+
+        xshape = x.size()
+        x = x.view(xshape[0], xshape[1]*xshape[2], xshape[3], xshape[4])
+
+        outputs = []
+
+        for i in range(4):
+            rotated_weight = torch.rot90(self.weight, i, [2, 3])
+            output = F.conv2d(x, rotated_weight, bias=self.bias, padding='same')
+            outputs.append(output)
+
+        return torch.stack(outputs, dim=2)
+
+
+'''
+class Z2P4GConvNet(nn.Module):
 
     def __init__(self):
 
-        super(P4GConvNet, self).__init__()
+        super(Z2P4GConvNet, self).__init__()
         self.gconv1 = P4GConv2d(in_channels=3, out_channels=8, kernel_size=3)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.gconv2 = P4GConv2d(in_channels=32, out_channels=16, kernel_size=3)
@@ -50,3 +75,4 @@ class P4GConvNet(nn.Module):
         x = self.fc(x)
 
         return x
+'''
