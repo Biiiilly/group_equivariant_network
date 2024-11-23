c20c531689c9f04a40cf05731c9a84edff525e13
Biiiilly <bm221@ic.ac.uk>
2024-11-23 00:30:24 +0000

    check_equivariance

--- gcnn_p4/check_equivariance.py
+++ gcnn_p4/check_equivariance.py
@@ -10,9 +10,9 @@ Approach 2: put the image into the network and then rotate the reuslt.
 So it's equivalent to check whether these two approaches give the same results.
 '''
 
-def check_equivariant(network, batch_size):
+def check_equivariant(network, in_channel, out_channel, image_size, batch_size):
 
-    x = torch.randn(batch_size, 3, 28, 28)
+    x = torch.randn(batch_size, in_channel, image_size, image_size)
     result = network(x)
 
     for i in range(1, 4):
@@ -21,28 +21,52 @@ def check_equivariant(network, batch_size):
         x_rotate = torch.rot90(x, i, (2, 3))
         rotate_result = network(x_rotate)
 
-        assert torch.allclose(torch.sort(result_rotate, dim=2)[0], torch.sort(rotate_result, dim=2)[0], atol=1e-8)
+        for b in range(batch_size):
+            for c in range(out_channel):
+
+                result_rotate_fixed = result_rotate[b][c].reshape(4, -1)
+                rotate_result_fixed = rotate_result[b][c].reshape(4, -1)
+
+                first_column1 = result_rotate_fixed[:, 0]
+                first_column2 = rotate_result_fixed[:, 0]
+
+                sorted_indices1 = torch.argsort(first_column1)
+                sorted1 = result_rotate_fixed[sorted_indices1]
+                sorted_indices2 = torch.argsort(first_column2)
+                sorted2 = rotate_result_fixed[sorted_indices2]
+
+                for ind in range(4):
+                    if torch.allclose(sorted1[i], sorted2[i], atol=1e-6) == False:
+                        print(f'b:{b}, c:{c}, i:{i}')
+                        print(sorted1[i])
+                        print(sorted2[i])
+                        difference = torch.abs(sorted1[i] - sorted2[i])
+                        print("Max absolute difference:", torch.max(difference))
+                    assert torch.allclose(sorted1[i], sorted2[i], atol=1e-5)
 
 
 class test_P4GConvNet(nn.Module):
 
-    def __init__(self):
+    def __init__(self, in_channels, out_channels, kernel_size):
 
         super(test_P4GConvNet, self).__init__()
-        # self.batck_size = batch_size
-        self.gconv1 = g.Z2P4GConv2d(in_channels=3, out_channels=8, kernel_size=3)
-        self.gconv2 = g.P4P4GConv2d(in_channels=8, out_channels=16, kernel_size=3)
+        self.gconv1 = g.Z2P4GConv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
 
     def forward(self, x):
 
         x = self.gconv1(x)
         x = g.GConv2d_MaxPooling(x, 2)
-        x = self.gconv2(x)
-        x = g.GConv2d_MaxPooling(x, 2)
 
         return x
-    
+
+
+
 batch_size = 64
-model = test_P4GConvNet()
-check_equivariant(model, batch_size)
+in_channels = 3
+out_channels = 64
+kernel_size = 3
+image_size = 28
+
+model = test_P4GConvNet(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
+check_equivariant(model, in_channel=in_channels, out_channel=out_channels, image_size=image_size, batch_size=batch_size)
 print('True')
